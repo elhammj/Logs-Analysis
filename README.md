4f0a78c2cf06a6f3a8ee89ecd3bb33e125182d7f
vagrant <vagrant@vagrant.vm>
2018-10-30 12:15:16 +0000

    Improve log.py and readme files

--- README.md
+++ README.md
@@ -4,14 +4,21 @@
 It is a Full Stack project that emphsizes python &  SQL skills. 
 
 ## Overview:
-The tool prints out reports (in plain text) based on the data in the database. It has three questions and the tool answers them by using specific SQL queries. 
+This project sets up a PostgreSQL database for a news website to generate a report.
+The provided Python script (log.py) uses the psycopg2 library to query the database and
+produce a report that answers the following questions:
+Q1: What are the most popular three articles of all time?
+Q2: Who are the most popular article authors of all time?
+Q3: On which days did more than 1% of requests lead to errors?
+The tool prints out reports (in plain text) based on the data in the database. 
 
 ## Files:
 1. **newsdata.sql**: News database which consists of three tabels:
 * The `authors` table includes information about the authors of articles
 * The `articles` table includes the articles themselves
 * The `log` table includes one entry for each time a user has accessed the site
-2. **log.py**: It is a python 3 code that runs the SQL queries by using psycopg2 library
+2. **log.py**: It is a python 3 code that runs the SQL queries 
+by using psycopg2 library
 3. **output.txt**: It is a text fiel that contains the output from log.py file
 
 ## To Run the tool:
--- log.py
+++ log.py
@@ -27,19 +27,45 @@ quesiton_3 = "On which days did more than 1% of requests lead to errors?"
 # The CONCAT() function adds two or more expressions together.
 # date_trunc to group the time by day
 # round to round the numbers
-query_to_answer_1 = """select title, count(title) as views
-from articles,log where log.status = '200 OK' and
-log.path = concat('/article/',articles.slug)
-group by title order by views desc limit 3;"""
-query_to_answer_2 = """select name, count(author) as views
-from articles,log, authors where log.status = '200 OK'
-and log.path = concat('/article/',articles.slug) and
-articles.author = authors.id group by name order by views desc;"""
-query_to_answer_3 = """select to_char(day,'Mon DD,YYYY'), error_percentage
-from (select date_trunc('day', time) as day,round(100.0*sum(
-case log.status when '404 NOT FOUND'then 1 else 0 end)
-/count(log.status),3) as error_percentage
-from log  group by 1 order by 1) subq where error_percentage > 1;"""
+query_to_answer_1 = """
+SELECT title,
+       count(title) AS views
+FROM articles,
+     log
+WHERE log.status = '200 OK'
+  AND log.path = concat('/article/', articles.slug)
+GROUP BY title
+ORDER BY views DESC
+LIMIT 3;
+"""
+
+query_to_answer_2 = """
+SELECT name,
+       count(author) AS views
+FROM articles,
+     log,
+     authors
+WHERE log.status = '200 OK'
+  AND log.path = concat('/article/', articles.slug)
+  AND articles.author = authors.id
+GROUP BY name
+ORDER BY views DESC;
+"""
+
+query_to_answer_3 = """
+SELECT to_char(DAY, 'Mon DD,YYYY'),
+       error_percentage
+FROM
+  (SELECT date_trunc('day', TIME) AS DAY,
+          round(100.0*sum(CASE log.status
+                              WHEN '404 NOT FOUND' THEN 1
+                              ELSE 0
+                          END)/count(log.status), 3) AS error_percentage
+   FROM log
+   GROUP BY 1
+   ORDER BY 1) subq
+WHERE error_percentage > 1;
+"""
 
 
 # getResult: To pass a request and return a result
@@ -92,17 +118,19 @@ def answer_question_3(query):
         file.write(str(a[0]) + ' -- ' + str(round(a[1], 2)) + '%' + "\n")
 
 
-# print out the answers
-answer_question_1(query_to_answer_1)
-# to get space between questions
-file.write("" + "\n")
-print("")
-answer_question_2(query_to_answer_2)
-# to get space between questions
-file.write("" + "\n")
-print("")
-answer_question_3(query_to_answer_3)
-print("")
-print("""The questions and their answers have been already saved
-in output.txt file, in black/white format !""")
-file.close()
+# To makes sure that this file was run directly, not imported
+if __name__ == '__main__':
+    # print out the answers
+    answer_question_1(query_to_answer_1)
+    # to get space between questions
+    file.write("" + "\n")
+    print("")
+    answer_question_2(query_to_answer_2)
+    # to get space between questions
+    file.write("" + "\n")
+    print("")
+    answer_question_3(query_to_answer_3)
+    print("")
+    print("""The questions and their answers have been already saved
+    in output.txt file, in black/white format !""")
+    file.close()
